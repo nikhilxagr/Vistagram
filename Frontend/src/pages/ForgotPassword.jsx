887c405abcbd0c92de3f1aa140c58bc5e1b073f4
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-07-28 23:42:35 +0530

    Adding Reset Password  Functionality and signIn page

---
+++ Frontend/src/pages/ForgotPassword.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ForgotPassword() {
+  return (
+    <div className="w-full h-screen bg-gradient-to-b from-black to-gray-900 flex flex-col justify-center items-center">
+      <div className="w-[90%] max-w-[1150px] h-[600px] bg-white rounded-2xl flex justify-center items-center overflow-hidden border-2 border-[#1a1f23]">
+      
+    </div>
+    </div>
+    )
+}
+
+export default ForgotPassword;
